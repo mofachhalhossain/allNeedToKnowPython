caf2368297fada516d92da2f8059d465a13dcbd5
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-09-01 17:14:31 +0600

    backslash character and escape sequence

--- allNeedToKnowPython.py
+++ allNeedToKnowPython.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+
+
+''' backslash characters/escape sequence and comment '''
+# single line comment
+'''Multiple
+    Line
+    Comment.'''
+
+print("Hello People!\n Welcome to Python")
+''' for tab \t, for print double quote (" \"\" ") '''
+print("Name: \"John Smith\", \n Age: \"23\"")
