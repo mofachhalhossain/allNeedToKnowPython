c89b4461e5aa3fbc24022e8a03554945b978c5d7
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-09-01 17:20:24 +0600

    identity operators

--- allNeedToKnowPython.py
+++ allNeedToKnowPython.py
@@ -66,3 +66,17 @@ print(a or b)
 
 # Print not a is False
 print(not a)
+
+
+''' Identity Operators '''
+
+''' (is and is not) are the identity operators both are used to check if two values 
+are located on the same part of the memory. Two variables that are equal do not 
+imply that they are identical. '''
+
+a = 10
+b = 20
+c = a
+
+print(a is not b)
+print(a is c)
