150f76b2f38dd4bbca0b388590d114938962a683
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-09-01 17:15:57 +0600

    variables

--- allNeedToKnowPython.py
+++ allNeedToKnowPython.py
@@ -10,3 +10,19 @@ print("Hello World!")
 print("Hello People!\n Welcome to Python")
 ''' for tab \t, for print double quote (" \"\" ") '''
 print("Name: \"John Smith\", \n Age: \"23\"")
+
+
+''' variables '''
+''' In other programming languages like C, C++, and Java, you will need to declare the
+ type of variables but in Python you don’t need to do that. Just type in the variable
+  and when values will be given to it, then it will automatically know whether the 
+  value given would be an int, float, or char or even a String. '''
+
+name = "Richi Ria";
+age = 23;
+college = "University of Valencia";
+gpa = "69%"
+''' age type conversion int to string  '''
+print("Name: "+name+" Age: "+str(age)+" College: "+college+" GPA: "+gpa)
+''' no type conversion '''
+print("Name: "+name+" Age: ",age," College: "+college+" GPA: "+gpa)
