2e9b52d66b0c5f324b45ec15c11f4751c8fa1a77
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-09-01 17:18:58 +0600

    ex. logical operators

--- allNeedToKnowPython.py
+++ allNeedToKnowPython.py
@@ -53,3 +53,16 @@ less than or equals <= '''
 ''' and	-> Logical AND: True if both the operands are true	x and y
 or ->	Logical OR: True if either of the operands is true 	x or y
 not ->	Logical NOT: True if operand is false 	not x '''
+
+# Examples of Logical Operator
+a = True
+b = False
+
+# Print a and b is False
+print(a and b)
+
+# Print a or b is True
+print(a or b)
+
+# Print not a is False
+print(not a)
